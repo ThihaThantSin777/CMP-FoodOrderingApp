a732864716e5a9c9a9c078da752e3b671de39543
Thiha Thant Sin <42347119+ThihaThantSin777@users.noreply.github.com>
2025-11-15 09:30:06 +0700

    Add database builder for ios and android

--- composeApp/src/iosMain/kotlin/org/thiha/thant/sin/foa/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/thiha/thant/sin/foa/MainViewController.kt
@@ -1,5 +1,9 @@
 package org.thiha.thant.sin.foa
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.thiha.thant.sin.foa.persistence.getDatabaseBuilderIOS
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    val databaseBuilder = getDatabaseBuilderIOS()
+    App(databaseBuilder)
+}
